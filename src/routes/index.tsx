6c7a62378f30e57fe9a2f479ce2a7029d6301102
Ayush Mathur <ayumathur9@gmail.com>
2026-05-28 11:40:13 +0530

    chore: remove meta pixel

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -58,19 +58,7 @@ export const Route = createFileRoute("/")({
           performer: { "@type": "Person", name: "Dr. Shivangi Goyal" },
         }),
       },
-      // Meta Pixel Code
-      {
-        children: `!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '638814107950963');
-fbq('track', 'PageView');`,
-      },
+
     ],
   }),
 });
@@ -99,15 +87,6 @@ function LandingPage() {
 
   return (
     <div className="relative min-h-screen bg-background text-foreground antialiased overflow-x-hidden">
-      <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: "none" }}
-          src="https://www.facebook.com/tr?id=638814107950963&ev=PageView&noscript=1"
-          alt=""
-        />
-      </noscript>
       <Nav scrolled={scrolled} />
       <Urgency />
       <Hero />
